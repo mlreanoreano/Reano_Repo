48d6fb49d3d098b53d5f468d6d68d8707c932510
Melvin Reano <mlreano@firstasia.edu.ph>
2025-02-14 16:02:09 +0800

    Reano, Melvin - acitvity_4.js

--- activity_4.js
+++ activity_4.js
@@ -1,3 +1,4 @@
+//Step 1
 let num1 = parseFloat(prompt("Enter the first number:"));
 let num2 = parseFloat(prompt("Enter the second number:"));
 let operator = prompt("Enter an operator (+, -, *, /):");
