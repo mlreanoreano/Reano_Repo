2184bbd470b302c7ae9a6673002a273ca26b78da
Melvin Reano <mlreano@firstasia.edu.ph>
2025-02-14 15:45:40 +0800

    initial commit activity_4.js

---
+++ activity_4.js
@@ -0,0 +1,28 @@
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let operator = prompt("Enter an operator (+, -, *, /):");
+
+if (isNaN(num1) || isNaN(num2)) {
+    console.log("Please enter valid numbers.");
+} else {
+    switch (operator) {
+        case '+':
+            console.log(`${num1} + ${num2} = ${num1 + num2}`);
+            break;
+        case '-':
+            console.log(`${num1} - ${num2} = ${num1 - num2}`);
+            break;
+        case '*':
+            console.log(`${num1} * ${num2} = ${num1 * num2}`);
+            break;
+        case '/':
+            if (num2 === 0) {
+                console.log("Cannot divide by zero");
+            } else {
+                console.log(`${num1} / ${num2} = ${num1 / num2}`);
+            }
+            break;
+        default:
+            console.log("Invalid operator");
+    }
+}
